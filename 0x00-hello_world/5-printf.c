6a26b2123b56fbbc9acf1113b9a91c98e1163c2e
TG Ebiri <ebiritg@gmail.com>
2023-02-16 18:48:18 -0800

    Create file 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*Entry point*/
 int main(void)
 {
+	/**
+	 * Description: The entry point of,
+	 * the program.
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
