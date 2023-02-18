3314227d55576ce2f0a435f6fbf005117388adb1
TG Ebiri <ebiritg@gmail.com>
2023-02-18 12:24:51 -0800

    Print some specified text

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  * Description: This function prints out a specified text".
  * Return: 1.
  */
- int main(void)
+int main(void)
 {
-	fprintf( stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, " and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19");
 	return (1);
 }
