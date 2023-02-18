a2394ef7fea3e48551fa681ab707a38e1468f7a4
TG Ebiri <ebiritg@gmail.com>
2023-02-18 07:03:32 -0800

    Print size of datatypes in computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of char: %2d byte(s)\n", sizeof(char));
+	printf("Size of int: %2d byte(s)\n", sizeof(int));
 	printf("Size of long int: %2d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %4d byte(s)\n", sizeof(long long int));
+	printf("Size of long long int: %2d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %2d byte(s)\n", sizeof(float));
 	return (0);
 }
