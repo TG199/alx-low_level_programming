abecd9dbca25ee4f35512de2b95f0eba19264e01
TG Ebiri <ebiritg@gmail.com>
2023-02-18 09:30:33 -0800

    Print some specified text with fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
-int main(void)
+/**
+ * main - Entry point of program
+ *
+ * Description: This function prints out a specified text".
+ * Return: 1.
+ */
+ int main(void)
 {
-	fprintf(stderr,""and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
-
-		return (1);
+	fprintf(stderr,""and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+	return (1);
 }
