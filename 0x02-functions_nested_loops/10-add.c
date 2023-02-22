9c843beb6fd7cb0e69513a88a436176a9d6e4887
TG Ebiri <ebiritg@gmail.com>
2023-02-21 17:46:52 -0800

    Add 2 numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
  * add - Entry point of program
+ *@i: first arg.
+ *@j: sconf arg.
  *
  * Description: The functon add 2 numbers.
  * Return: 0.
  */
-int add(int a, int b)
+int add(int i, int j)
 {
-	return (a + b);
+	return (i + j);
 }
