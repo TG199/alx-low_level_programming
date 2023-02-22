00c3d9c2c9d24340fd8df20f9bc9b6b19166ec2d
TG Ebiri <ebiritg@gmail.com>
2023-02-21 17:13:11 -0800

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	print_alphabet();
+	 _putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: The functon add 2 numbers.
+ * Return: 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = add(60, 40);
+	printf("%d\n", n);
+	return (0);
+}
+
