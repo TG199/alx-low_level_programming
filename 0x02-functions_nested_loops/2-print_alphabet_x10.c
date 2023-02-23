c7da3b9bbcad9801a3ca2571463f2ceb3441351f
TG Ebiri <ebiritg@gmail.com>
2023-02-22 20:14:41 -0800

    Print lowercase alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,15 @@
  */
 void print_alphabet(void)
 {
-	char ch[27] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-	int r = 0;
+	char ch = 'a',
 
-	while (r <= 10)
+	while (ch <= 'z')
 	{
-		_putchar(ch);
-		ch++;
-	} _putchar('\n');
+		for (int i = 0; i <= 10; i++)
+		{
+			_putchar(ch);
+			ch++;
+		} _putchar('\n');
+	}
 }
 
