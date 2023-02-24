cb74c680bb2e02e2659c34c3a99804e99f5bcf4a
TG Ebiri <ebiritg@gmail.com>
2023-02-24 15:39:53 -0800

    Print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Print_numbers - print 0 -9.
+ *
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while(i < 10)
+	{
+		_putchar(i);
+	} i++;
+	_piuchar('\n');
+}
