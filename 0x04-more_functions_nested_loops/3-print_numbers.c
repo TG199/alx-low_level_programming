89d16031fdd1dc2791b402239ae41898ec372d57
TG Ebiri <ebiritg@gmail.com>
2023-02-24 15:42:03 -0800

    Print 0 - 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 /**
  * Print_numbers - print 0 -9.
  *
+ * Description: This function prints 0 -9.
  * Return: 0.
  */
 void print_numbers(void)
 {
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		_putchar(i);
 	} i++;
