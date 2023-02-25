252faf57bdeff781ef9ad58d7d9a29dd78489814
TG Ebiri <ebiritg@gmail.com>
2023-02-25 14:14:30 -0800

    Print 1 - 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,13 @@ void more_numbers(void)
 	int i = '0';
 	int j;
 
-	while (i <= 10)
+	for (i = 0; i <= 10; i++)
 	{
 		for (j = '0'; j <= 14; j++)
 		{
 			_putchar(j);
 		}
 		_putchar(i);
-		i++;
 		_putchar('\n');
 	}
 }
