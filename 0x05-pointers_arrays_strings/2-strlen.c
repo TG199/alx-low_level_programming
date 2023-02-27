2fdaff7c1f348836e3e1c8831a1eb055d4ec8e84
TG Ebiri <ebiritg@gmail.com>
2023-02-27 15:44:38 -0800

    Find length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Return length of string
+ * @s: string parameter
+ *
+ * Description:This function prints the length of string.
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	while (s[length] != '\0')
+	length++;
+	return length;
+}
+
