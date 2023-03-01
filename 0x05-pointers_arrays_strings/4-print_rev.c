e998d217ff262d2cfa6880d9e84c8af451dc6333
TG Ebiri <ebiritg@gmail.com>
2023-02-28 18:37:37 -0800

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,15 @@
  */
 void print_rev(char *s)
 {
-	if (*s != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		print_rev(s + 1);
-		_putchar(*s);
+		i++;
 	}
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	} _putchar('\n');
 }
-_putchar('\n');
+
