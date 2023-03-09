6c69c7e0fe869c590db1b05ef784ec4667f90e66
TG Ebiri <ebiritg@gmail.com>
2023-03-09 00:31:14 -0800

    Find factorial of given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - find factorial of number
+ * @n: number to find
+ *
+ * Description: This function finds the factorial
+ * of a given number
+ * Return: -1 (error)
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n  = n * factorial(n - 1));
+	}else if(n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
