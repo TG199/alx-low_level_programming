478542ccb9f3e832bbacde850ef1b3cdf22d48dc
TG Ebiri <ebiritg@gmail.com>
2023-03-15 07:14:19 -0700

    Write a program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,11 @@
 /**
  * main - print number of args
  * @argc: number of args
+ * @argv: NULL
  *
  * Description: This functio prints the nmber of args
  * passed into it
+ * Return: 0;
  */
 int main(int argc, char *argv[])
 {
