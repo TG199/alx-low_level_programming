0ff45a5b61a6c4d0cbc03aa6b1b0e8ae07446f61
TG Ebiri <ebiritg@gmail.com>
2023-03-20 18:11:58 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#endif
+#include "main.h"
+
+/**
+ * struct my_dog - create struct
+ * @name: first member
+ * @age: second member
+ * @owner third member
+ *
+ * Description: Create struct variable
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
