0c077397209acf79f977d034900a0b453457276d
TG Ebiri <ebiritg@gmail.com>
2023-04-21 13:06:44 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+
+/**
+ * _strlen - Print length of string
+ * @s: String to return
+ *
+ * Description: This function counts the length of a given
+ * string and returns the length
+ * Return: length
+ */
+size_t _strlen(const char *s)
+{
+	ssize_t index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+
 /**
  * create_file - creates a file
  * @filename: name of file to create
@@ -14,32 +34,33 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t write_file;
 
-
 	if (filename == NULL)
 	{
 		return (1);
 	}
 	else if (text_content == NULL)
 	{
-		if ((fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600)) == -1)
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
 		{
 			return (-1);
 		}
 	}
 	else
 	{
-	
-		if ((fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600)) == -1)
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
 		{
 			return (-1);
 		}
 
-		if ((write_file = write(fd, text_content, _strlen(text_content))) == -1)
+		write_file = write(fd, text_content, _strlen(text_content));
+		if (write_file == -1)
 		{
 			return (-1);
 		}
 	}
-	
+
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,7 +10,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-size_t _strlen(const char *s);
 
 
 
