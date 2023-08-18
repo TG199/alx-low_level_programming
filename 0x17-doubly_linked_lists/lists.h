97717eec1a6a565388af321756b63fd19f4003a1
TG <ebiritg@gmail.com>
2023-08-17 18:03:41 -0700

    Add header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
