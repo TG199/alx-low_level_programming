77ac9b229adffb2a45f02515b157891c88b5e890
Kelechi <ebirikele@gmail.com>
2023-11-12 19:15:18 +0100

    fix betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate  number of string 
+ * _strncat - concatenate  number of string
  * @dest: string to append to
  * @src: string to append
- * 
+ * @n: no of char to append
+ *
  * Description: This function appends a number of specified number of string
  * from the source string to the destination string
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i = 0, j = 0;
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-    while (src[j] != '\0' && j < n)
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-    return (dest);
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
