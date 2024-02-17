72455995abfa2880969352dd4647f96881356bc2
Kelechi <ebirikele@gmail.com>
2024-02-17 11:05:58 +0100

    Write a hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,7 +44,7 @@ typedef struct node
 
 hash_node_t *create_item(const char *key, const char *value);
 
-/*Hash DFunctions*/
+/*Hash Functions*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
