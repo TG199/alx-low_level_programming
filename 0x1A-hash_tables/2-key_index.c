888e9ffa889b54175df9726a5863a2edd92c599a
Kelechi <ebirikele@gmail.com>
2024-02-22 03:39:40 +0100

    try a different approach

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of  key
+ * @key: key
+ * @size: size
+ *
+ * Return: index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+	if (key == NULL)
+		return (0);
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
